ed61b9e46d227e7b27860789a975f0bac8023e32
Tonyngor <123629176+Tonyngor@users.noreply.github.com>
2023-02-11 22:13:54 +0300

    Изменения 004 Задача 8

---
+++ Example004_HelloConsole/Program.cs
@@ -0,0 +1,20 @@
+﻿Console.Write("Введите число n: ");
+int n = int.Parse(Console.ReadLine());
+int x = 2;
+if (n < 2)
+{
+
+    Console.WriteLine("Ошибка!");
+
+}
+else
+{
+
+    while (x < n + 1)
+    {
+
+        Console.WriteLine(x);
+        x = x + 2;
+    }
+
+}
