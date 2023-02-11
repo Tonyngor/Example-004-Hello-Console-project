dec635a302c3ece212917be1806143eade01c724
Tonyngor <123629176+Tonyngor@users.noreply.github.com>
2023-02-11 12:39:33 +0300

    Example 001 Hello Console project

---
+++ Example001_HelloConsole/Program.cs
@@ -0,0 +1,21 @@
+﻿Console.Write("Введите число a: ");
+double a = double.Parse(Console.ReadLine());
+Console.Write("Введите число b: ");
+double b = double.Parse(Console.ReadLine());
+
+
+if (a < b)
+  {
+  double Result2 = a;
+   double Result = b;
+     Console.Write("Максимальное число - "); Console.WriteLine(Result);
+     Console.Write("Минимальное число - "); Console.WriteLine(Result2);
+  }
+  else
+  {
+    double Result = a;
+    double Result2 = b;
+
+    Console.Write("Максимальное число - "); Console.WriteLine(Result);
+    Console.Write("Минимальное число - "); Console.WriteLine(Result2);
+  }
