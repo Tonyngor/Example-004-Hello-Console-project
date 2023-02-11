5e31137cf6823c1640059e371beff9d151db2fb1
Tonyngor <123629176+Tonyngor@users.noreply.github.com>
2023-02-11 14:20:36 +0300

    изменение 003 задача 6

---
+++ Example003_HelloConsole/Program.cs
@@ -0,0 +1,15 @@
+﻿Console.Write("Введите число a: ");
+double a = double.Parse(Console.ReadLine());
+
+
+if (a % 2 == 0)
+{
+    Console.WriteLine("Число четное!");
+
+}
+else
+{
+    Console.WriteLine("Нет!");
+
+}
+
