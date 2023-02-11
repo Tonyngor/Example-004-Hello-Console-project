56d94e131914fc68b3f8118f075d746ac7a1f26a
Tonyngor <123629176+Tonyngor@users.noreply.github.com>
2023-02-11 13:53:11 +0300

    добавили 002 задача 4

---
+++ Example002_HelloConsole/Program.cs
@@ -0,0 +1,32 @@
+﻿Console.Write("Введите число a: ");
+double a = double.Parse(Console.ReadLine());
+Console.Write("Введите число b: ");
+double b = double.Parse(Console.ReadLine());
+Console.Write("Введите число c: ");
+double c = double.Parse(Console.ReadLine());
+double d = 0;
+
+
+if (a > b)
+{
+    d = a;
+
+
+}
+else
+{
+    d = b;
+
+}
+
+if (d > c)
+{
+    d = d;
+}
+else
+{
+    d = c;
+}
+double Result = d;
+
+Console.WriteLine("Максимальное число - "); Console.Write(Result);
